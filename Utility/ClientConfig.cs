971357a35ce45550637df2b9114bbd51e97ff8e4
TheAshenWolf <jirirkas.skola@gmail.com>
2023-12-23 21:24:12 +0100

    Added a client config containing XP Bar position settings

---
+++ Utility/ClientConfig.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace LevelPlus.Utility;
+
+public class ClientConfig : ModConfig
+{
+  public static ClientConfig Instance => ModContent.GetInstance<ClientConfig>();
+  
+  public override ConfigScope Mode => ConfigScope.ClientSide;
+  
+  [DefaultValue(true)]
+  public bool xpBarLocked = true;
+  
+  [DefaultValue(480)]
+  public int xpBarLeft;
+  
+  [DefaultValue(35)]
+  public int xpBarTop;
+}
