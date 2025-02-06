a057cd4a392c74a6431c0693c62ddf12f5e39656
Nathaniel Sunday <nathille333@gmail.com>
2025-02-06 04:14:18 -0600

    Update Packet.cs
    Reworked Packet

--- Network/Packet.cs
+++ Network/Packet.cs
@@ -1,8 +1,36 @@
+using System.IO;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace LevelPlus.Network;
 
-public interface Packet
+public abstract class Packet : ModType
 {
-    void Handle(Mod mod, int whoAmI);
+    protected abstract void Write(BinaryWriter writer);
+    public abstract void Handle(BinaryReader reader, int whoAmI);
+
+    public void Send(int toClient = -1, int ignoreClient = -1)
+    {
+        ModPacket packet = Mod.GetPacket();
+        
+        packet.Write(GetType().Name);
+        Write(packet);
+        
+        packet.Send(toClient, ignoreClient);
+    }
+    
+    public void Spread()
+    {
+        switch (Main.netMode)
+        {
+            case NetmodeID.MultiplayerClient:
+                Send(-1, Main.myPlayer);
+                break;
+            
+            case NetmodeID.Server:
+                Send();
+                break;
+        }
+    }
 }
