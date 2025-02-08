9bbe969de84f1c27da999b06d9dbec4c530007ab
Nathaniel Sunday <nathille333@gmail.com>
2025-02-08 06:48:12 -0600

    Update LevelPlus.cs

--- LevelPlus.cs
+++ LevelPlus.cs
@@ -1,15 +1,52 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using LevelPlus.Network;
 using Terraria.ModLoader;
 
-namespace LevelPlus
-{
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class LevelPlus : Mod
-	{
+namespace LevelPlus;
 
-	}
+public class LevelPlus : Mod
+{
+    public string LocalizationPrefix => "Mods." + Name + ".";
+    public string AssetPath => Name + "/Assets/";
+    public bool IsCalamityLoaded { get; private set; }
+    public bool IsThoriumLoaded { get; private set; }
+
+    public override void Load()
+    {
+        IsCalamityLoaded = ModLoader.HasMod("CalamityMod");
+        IsThoriumLoaded = ModLoader.HasMod("ThoriumMod");
+        
+        Logger.InfoFormat("{0}: Calamity = {1}", Name, IsCalamityLoaded);
+        Logger.InfoFormat("{0}: Thorium = {1}", Name, IsThoriumLoaded);
+    }
+
+    public override void Unload()
+    {
+        IsCalamityLoaded = false;
+        IsThoriumLoaded = false;
+    }
+    
+    public override void HandlePacket(BinaryReader reader, int whoAmI)
+    {
+        var typeString = reader.ReadString();
+        var type = Type.GetType(typeString);
+        
+        if (type is null || 
+            !type.IsAssignableFrom(typeof(Packet)))
+        {
+            Logger.WarnFormat("{0}: Packet of unknown type \"{1}\"", Name, typeString);
+            return;
+        }
+
+        var packet = (Packet)Activator.CreateInstance(type);
+        
+        if (packet is null)
+        {
+            Logger.WarnFormat("{0}: Failed to instantiate Packet of type \"{1}\"", Name, typeString);
+            return;
+        }
+        
+        packet.Receive(reader, whoAmI);
+    }
 }
