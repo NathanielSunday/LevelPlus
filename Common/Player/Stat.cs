361b3fd69bd036ddac66d62a22cc07aa381963c4
Nathaniel Sunday <nathille333@gmail.com>
2025-02-13 03:12:39 -0600

    Update Stat.cs

--- Common/Player/Stat.cs
+++ Common/Player/Stat.cs
@@ -1,28 +1,84 @@
+using System;
+using LevelPlus.Common.System;
+using LevelPlus.Network;
 using Microsoft.Xna.Framework;
 using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace LevelPlus.Common.Player;
 
-public interface Stat
+// A class to make developing stats faster
+public abstract class Stat : ModPlayer
 {
+    // The value of, or amount of points invested in, the stat.
+    public int Value { get; set; }
+
+    // The value that would be while spending
+    public int ProjectedValue
+    {
+        get
+        {
+            var keybind = ModContent.GetInstance<KeybindSystem>();
+            return Value + Math.Min(ModContent.GetInstance<StatSystem>().Points,
+                (keybind.SpendMultFive.Current ? 5 : 1) *
+                (keybind.SpendMultTen.Current ? 10 : 1) *
+                (keybind.SpendMultTwenty.Current ? 20 : 1)
+            );
+        }
+    }
+
     // The LocalizedText for the name of the Stat
-    LocalizedText Name { get; }
-    
+    public virtual LocalizedText Name =>
+        Language.GetText(((LevelPlus)Mod).LocalizationPrefix + "Stats." + Id + ".DisplayName");
+
     // The LocalizedText for the description. Should be pre-formatted with args.
-    LocalizedText Description { get; }
-    
+    public virtual LocalizedText Description =>
+        Language.GetText(((LevelPlus)Mod).LocalizationPrefix + "Stats." + Id + ".Tooltip");
+
     // The LocalizedText for the description for next point(s) spent. Should be pre-formatted with args.
-    LocalizedText SpendTooltip { get; }
-    
-    // The access key for stat, usually the name.
-    string Id { get; }
-    
+    public virtual LocalizedText SpendTooltip => Description;
+
     // The path of the icon to be used in the UI.
-    string IconPath { get; }
+    public virtual string IconPath => ((LevelPlus)Mod).AssetPath + "Icons/" + Id;
     
     // The color to modify the UI element by.
-    Color Color { get; }
+    public virtual Color Color => Color.White;
     
-    // The value of, or amount of points invested in, the stat.
-    int Value { get; set; }
+    // The access key for stat, usually the name.
+    public abstract string Id { get; }
+    
+    public override void LoadData(TagCompound tag)
+    {
+        Value = tag.ContainsKey(Id) ? tag.GetInt(Id) : 0;
+    }
+
+    public override void SaveData(TagCompound tag)
+    {
+        tag.Add(Id, Value);
+    }
+
+    public override void CopyClientState(ModPlayer targetCopy)
+    {
+        ((Stat)targetCopy).Value = Value;
+    }
+
+    public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
+    {
+        if (!newPlayer) return;
+
+        var packet = new StatPacket
+        {
+            Id = Id,
+            Value = Value,
+        };
+
+        packet.Send(toWho, fromWho);
+    }
+
+    public override void SendClientChanges(ModPlayer clientPlayer)
+    {
+        if (((Stat)clientPlayer).Value == Value) return;
+        SyncPlayer(0, 0, true);
+    }
 }
